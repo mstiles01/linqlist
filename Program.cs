b331a639ed8c92a3fc3f187d3b601955963ce212
Michael Stiles <mstiles01@icloud.com>
2019-10-11 10:12:50 -0500

    Where method

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linqprac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Find the words in the collection that start with the letter 'L'
+List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+IEnumerable<string> LFruits = fruits.Where(word => word.StartsWith("L"));
+foreach (string fruit in LFruits) {
+    Console.WriteLine (fruit);
+}
+Console.WriteLine();
+        }
+    }
+}
